a34bd632a9adea69be74c44860840e1863e61bc3
DenisNedelko <dnsndlk@gmail.com>
2024-12-18 16:11:14 +0300

    feat: add solution for remove-from-list.js 2024-12-18 16:11:14

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,11 +22,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+	if (!l) return null;
+	if (l.value === k) {
+		return removeKFromList(l.next, k);
+	}
+	l.next = removeKFromList(l.next, k);
+	return l;
 }
 
 module.exports = {
-  removeKFromList
+	removeKFromList,
 };
